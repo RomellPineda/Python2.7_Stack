0ff3c2ff916775abb871e17cc4ce5e9a3fb8bbbc
RomellPineda <romell.pineda@gmail.com>
2018-11-22 11:17:58 -0800

    urls views css index

--- eats/apps/food_app/views.py
+++ eats/apps/food_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+# import bcrypt
+# import re
+# import stripe
 
 # Create your views here.
 def index(request):
-    response = "Hello, I am django preflight!"
-    return HttpResponse(response)
+    return render(request, 'food_app/index.html')
