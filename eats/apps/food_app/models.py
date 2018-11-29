ff61de417a740f8cdcf873dd712980cced4ea84c
RomellPineda <romell.pineda@gmail.com>
2018-11-28 21:44:31 -0800

    added forms

--- eats/apps/food_app/models.py
+++ eats/apps/food_app/models.py
@@ -2,14 +2,14 @@ from __future__ import unicode_literals
 from django.db import models
 from datetime import datetime
 
+
 class User(models.Model):
-    # admin = models.BooleanField(default=True)
+    admin = models.BooleanField(default=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
-    address_two = models.CharField(max_length=255)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zip_code = models.IntegerField()
@@ -20,19 +20,23 @@ class User(models.Model):
     def __str__(self):
         return self.first_name + ' | ' + self.last_name + ' | ' + self.email
 
+
+class Price(models.Model):
+    price = models.IntegerField()
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     quantity = models.IntegerField()
     description = models.TextField()
     purchaser = models.ManyToManyField(User, related_name = 'products')
-    price = models.ForeignKey(Price, related_name ='product_price')
+    charge = models.ForeignKey(Price, related_name ='cost')
+
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
-    product = models.ForeignKey(Product, related_name = 'ingredients')
-
-class Price(models.Model):
-    price = models.IntegerField()
+    product = models.ManyToManyField(Product, related_name = 'ingredients')
--- eats/apps/food_app/views.py
+++ eats/apps/food_app/views.py
@@ -1,9 +1,68 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
-# import bcrypt
-# import re
-# import stripe
+from django.db.models import Q
+from datetime import datetime
+from .models import *
+import bcrypt
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 # Create your views here.
 def index(request):
     return render(request, 'food_app/index.html')
+
+def register(request):
+    if request.method != 'POST':
+        return redirect('/')
+    error = False
+    if len(request.POST['first_name']) < 2:
+        messages.error(request,'Name must be 2 or more characters')
+        error = True
+    if not request.POST['first_name'].isalpha() or not request.POST['last_name'].isalpha():
+        messages.error(request,'First and last name must not contain numbers or spaces')
+        error = True
+    if not EMAIL_REGEX.match(request.POST['email']):
+        messages.error(request,'Invalid email')
+        error = True
+    if len(User.objects.filter(email = request.POST['email'])) > 0:
+        messages.error(request,'User already exsist')
+        error = True
+    if len(request.POST['password']) < 8:
+        messages.error(request,'Password must be 8 or more characters in length')
+        error = True
+    if request.POST['password'] != request.POST['confirm_pw']:
+        messages.error(request,'Password and confirm password must match')
+        error = True
+    if not request.POST['zip_code'].isnumeric():
+        messages.error(request, 'Zip code gotta be all numbers Sugar!')
+        error = True
+    if error:
+        return redirect('/')
+    else:
+        hashed = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+        user = User.objects.create(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email = request.POST['email'], password = hashed)
+        request.session['user_id'] = user.id
+        request.session['user_name'] = user.first_name
+        print(user.id)
+        print(user.first_name)
+    return redirect('/home')
+
+def login(request):
+    if request.method != 'POST':
+        return redirect('/')
+    user = User.objects.filter(email = request.POST['email'])
+    if len(user) > 0:
+        shmuser = user[0]
+        if bcrypt.checkpw(request.POST['password'].encode(), shmuser.password.encode()):
+            request.session['user_id'] = shmuser.id
+            request.session['user_name'] = shmuser.first_name
+            return redirect('/home')
+        else:
+            messages.error(request, 'Email or password invalid' )
+            return redirect('/')
+    else:
+        messages.error(request, 'Email or password invalid')
+        return redirect('/')
+
+def auth(request):
+    return render(request, 'food_app/reg.html')
